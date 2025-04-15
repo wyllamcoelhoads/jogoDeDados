880253e888e1980bbfed6acb88d53ff4f59a20fa
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2025-04-14 21:25:59 -0300

    pronto para jogar

--- Program.cs
+++ Program.cs
@@ -9,19 +9,25 @@ int total = roll1 + roll2 + roll3;
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll1 == roll3) || (roll1 == roll3)){
-    Console.WriteLine("You relled double! +2 bonus to total!");
-    total += 2;
+    
+    if ((roll1 == roll2 && roll2 == roll3)){
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }else{
+        Console.WriteLine("You relled double! +2 bonus to total!");
+        total += 2;
+    }
 } 
 
-if ((roll1 == roll2 && roll2 == roll3)){
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
- }
-
 if(total >= 15) {
-    Console.WriteLine($"You win! with: {total}");
+    Console.WriteLine($"You win a new car! with: {total}");
 }
-
-if (total < 15) {
-    Console.WriteLine("Sorry, you lose.");
+else if(total >= 10) {
+    Console.WriteLine($"You win a new laptop! with: {total}");
+}
+else if(total == 7) {
+    Console.WriteLine($"You win a trip for two! with: {total}");
+}
+else {
+    Console.WriteLine($"You win a kitten! with: {total}");
 }
